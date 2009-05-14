f978480b892781c90a24a196437bd5e1eb507f2e
Navjeet <navjeetc@gmail.com>
2009-05-13 23:18:17 -0400

    new batch file

---
+++ open_prompts.bat
@@ -0,0 +1,7 @@
+rem
+rem This batch file enables you to open three different dos windows with
+rem three different directories
+rem
+start cmd /k "c: && cd c:\irad\code\base"
+start cmd /k "c: && cd C:\java\apache-tomcat-6.0.18\bin"
+start cmd /k "c: && cd C:\apps\apache-activemq-5.1.0\bin"
